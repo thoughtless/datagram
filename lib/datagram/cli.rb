92d36bf76b3e1b48b025824518f389822bee86e9
Brad Gessler <brad@polleverywhere.com>
2013-05-23 16:56:08 -0700

    Moved migrate code into a class method so that the migration can be run from outside of the CLI.

--- lib/datagram/cli.rb
+++ lib/datagram/cli.rb
@@ -5,21 +5,7 @@ module Datagram
     desc "migrate [VERSION]", "Migrates the database schema"
     method_options %w( force -f ) => :boolean
     def migrate(version=nil)
-      # Load the database.
-      db = Datagram::Model.query_db
-
-      # Now load the extension.
-      Sequel.extension :migration
-
-      # If we're forcing the migrations, lets drop all the tables
-      if options[:force]
-        Datagram.logger.info "Destroying `#{db.uri}`"
-        db.destroy
-      end
-
-      # Now lets run the migrations
-      Datagram.logger.info "Migrating `#{db.uri}`"
-      Sequel::Migrator.apply(db, Datagram::Model::MIGRATION_PATH)
+      Datagram::Model.migrate(options)
     end
 
     desc "version", "Current version of datagram"
--- lib/datagram/model.rb
+++ lib/datagram/model.rb
@@ -15,6 +15,25 @@ module Datagram
       @query_db ||= Sequel.connect(ENV['QUERY_DATABASE_URL'] || QUERY_DATABASE_URL)
     end
 
+    # Migrate the Datagram query database.
+    def self.migrate(options={})
+      # Load the database.
+      db = Datagram::Model.query_db
+
+      # Now load the extension.
+      Sequel.extension :migration
+
+      # If we're forcing the migrations, lets drop all the tables
+      if options[:force]
+        Datagram.logger.info "Destroying `#{db.uri}`"
+        db.destroy
+      end
+
+      # Now lets run the migrations
+      Datagram.logger.info "Migrating `#{db.uri}`"
+      Sequel::Migrator.apply(db, Datagram::Model::MIGRATION_PATH)
+    end
+
     class Query < Sequel::Model(Datagram::Model.query_db)
     end
   end
